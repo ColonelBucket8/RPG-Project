f8b8d2cb8313935e47908e5d6178fa03e5c121ac
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-04-24 14:55:31 +0800

    Add mover script to player
    1. Navigate player to the target with navigation ai

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform target;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
