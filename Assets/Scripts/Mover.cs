d4434035b8176c8951f267b38bd312b2e5815029
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-04-24 17:03:44 +0800

    Add animation to player
    1. Three animations, idle, walk and running
    2. Change animation based on the speed of the navigation speed

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -11,6 +11,20 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
+
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+
+        // Animator is only interested in z velocity, moving forward
+        // The global value is not useful for the animator
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
 
     }
 
