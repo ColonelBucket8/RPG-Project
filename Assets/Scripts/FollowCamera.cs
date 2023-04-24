0eea12c28873b1faf84a95d79d0b2aa5b5eb0e29
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-04-24 17:13:04 +0800

    Tweak scripts
    1. Enable holding left click mouse button to move the player
    2. Fix some jittery camera movement

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,7 +5,9 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] Transform target;
 
 
-    void Update()
+    // There is a mismatch between player position and camera position 
+    // which causes camera position to be jittery if putting inside Update method
+    void LateUpdate()
     {
         transform.position = target.position;
     }
