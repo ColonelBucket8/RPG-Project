b25b76557e39b6017778c9445d56de681da0ed51
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-04 20:54:01 +0800

    Add fader script to fade in and fade out

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            // Nested coroutines
+            yield return FadeOut(3f);
+            print("Faded out");
+            yield return FadeIn(1f);
+            print("Faded in");
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                // moving alpha toward 1
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                // moving alpha toward 1
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+
+        }
+    }
+}
