baed3dd883489038ee2be32fe06e19575354e05c
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-07 19:10:09 +0800

    Save and load bytes from file path

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace RPG.Saving
@@ -7,12 +8,26 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            print("Saving to " + GetPathFromSaveFile(saveFile));
+            string path = GetPathFromSaveFile(saveFile);
+            print("Saving to " + path);
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("Hello world!");
+                stream.Write(bytes, 0, bytes.Length);
+            }
         }
 
         public void Load(string saveFile)
         {
-            print("Loading to " + GetPathFromSaveFile(saveFile));
+            string path = GetPathFromSaveFile(saveFile);
+            print("Loading from " + path);
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                print(Encoding.UTF8.GetString(buffer));
+            }
         }
 
         private string GetPathFromSaveFile(string saveFile)
