3bd1428178ddc1713e915a7f57d73864fb62956b
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-13 16:46:45 +0800

    Add uuid to saveable entities
    1. Only assign uuid the prefab instance that has saveable entities script

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,12 +1,16 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace RPG.Saving
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField] string uniqueIdentifier = "";
+
         public string GetUniqueIdentifier()
         {
-            return "";
+            return uniqueIdentifier;
         }
 
         public object CaptureState()
@@ -19,5 +23,27 @@ namespace RPG.Saving
         {
             print("Capturing state for: " + GetUniqueIdentifier());
         }
+
+        // Remove update method from build
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+
+            // Empty scene path means the editor is in prefab
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            // Only assign uuid to instance of prefab 
+            // To avoid uuid duplication
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
+
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                uniqueIdentifier = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+#endif
     }
 }
