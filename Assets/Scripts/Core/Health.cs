f84e7c6ef2a8c647f0b03ccfc23e997e67598056
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-14 17:31:34 +0800

    Add health to saveable entity

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,8 +1,9 @@
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float healthPoints = 20f;
 
@@ -49,6 +50,21 @@ namespace RPG.Core
                 capsuleCollider.enabled = false;
             }
 
+        }
+
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            healthPoints = (float)state;
+
+            if (healthPoints <= 0)
+            {
+                Die();
+            }
         }
     }
 }
