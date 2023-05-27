9b21fa04e9509704dac145117f92f61b187f0a65
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-28 01:58:31 +0800

    Add weapon animation

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -11,6 +11,7 @@ namespace RPG.Combat
         [SerializeField] float weaponDamage = 5f;
         [SerializeField] GameObject weaponPrefab = null;
         [SerializeField] Transform handTransform = null;
+        [SerializeField] AnimatorOverrideController weaponOverride = null;
 
         Health target;
         Animator animator;
@@ -54,6 +55,8 @@ namespace RPG.Combat
             if (weaponPrefab && handTransform)
             {
                 Instantiate(weaponPrefab, handTransform);
+                Animator animator = GetComponent<Animator>();
+                animator.runtimeAnimatorController = weaponOverride;
             }
         }
 
