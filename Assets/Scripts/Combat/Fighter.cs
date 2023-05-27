aea4214ab9e5193aa0c171e5a9ccc3b669bd0409
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-28 02:12:03 +0800

    Create a scriptable object sword

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,9 +9,8 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttack = 1f;
         [SerializeField] float weaponDamage = 5f;
-        [SerializeField] GameObject weaponPrefab = null;
         [SerializeField] Transform handTransform = null;
-        [SerializeField] AnimatorOverrideController weaponOverride = null;
+        [SerializeField] Weapon weapon = null;
 
         Health target;
         Animator animator;
@@ -52,11 +51,10 @@ namespace RPG.Combat
 
         private void SpawnWeapon()
         {
-            if (weaponPrefab && handTransform)
+            if (weapon != null)
             {
-                Instantiate(weaponPrefab, handTransform);
                 Animator animator = GetComponent<Animator>();
-                animator.runtimeAnimatorController = weaponOverride;
+                weapon.Spawn(handTransform, animator);
             }
         }
 
