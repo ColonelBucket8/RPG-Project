162a003ad4b08a8fc20dad21fd11dbe6ab9a373e
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-06-05 17:02:44 +0800

    Add respawn time for weapons after pickup

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -5,13 +8,33 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5f;
+
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                // Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<SphereCollider>().enabled = shouldShow;
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
             }
         }
     }
