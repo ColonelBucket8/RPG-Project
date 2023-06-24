bf63c4b0460dc10acdcd56a544bfbe534350ab53
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-06-24 15:44:27 +0800

    Show health bar if not full and alive

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -9,11 +9,30 @@ namespace RPG.Attributes
         Health healthComponent = null;
         [SerializeField]
         RectTransform foreground = null;
+        [SerializeField]
+        Canvas rootCanvas = null;
 
         void Update()
         {
+            showHealthBar();
+
             foreground.localScale =
                 new Vector3(healthComponent.GetFraction(), 1, 1);
+        }
+
+        private void showHealthBar()
+        {
+            float healthFraction = healthComponent.GetFraction();
+
+            if (!Mathf.Approximately(healthFraction, 1f) &&
+                !Mathf.Approximately(healthFraction, 0f))
+            {
+                rootCanvas.gameObject.SetActive(true);
+            }
+            else
+            {
+                rootCanvas.gameObject.SetActive(false);
+            }
         }
     }
 }
